96ed8fe8c7e201e18994f88806c8cfae493f32e9
Tajh Martin <tajhmartin87@gmail.com>
2020-12-08 13:58:30 -0600

    add turning

--- Raspi_MotorHAT/turningTest.py
+++ Raspi_MotorHAT/turningTest.py
@@ -12,36 +12,40 @@ backRight = mh.getMotor(4)
 duration = 2
 speed = 100
 
-
+def forward():
+    frontRight.run(Raspi_MotorHAT.FORWARD)
+    frontLeft.run(Raspi_MotorHAT.FORWARD)
+    backLeft.run(Raspi_MotorHAT.BACKWARD)
+    backRight.run(Raspi_MotorHAT.BACKWARD)
+    
 
 def stop():
-	frontRight.run(Raspi_MotorHAT.RELEASE)
-	frontLeft.run(Raspi_MotorHAT.RELEASE)
-	backLeft.run(Raspi_MotorHAT.RELEASE)
-	backRight.run(Raspi_MotorHAT.RELEASE)
+    frontRight.run(Raspi_MotorHAT.RELEASE)
+    frontLeft.run(Raspi_MotorHAT.RELEASE)
+    backLeft.run(Raspi_MotorHAT.RELEASE)
+    backRight.run(Raspi_MotorHAT.RELEASE)
 
 def turnRight():
-	frontLeft.setSpeed(speed)
-	backLeft.setSpeed(speed)
-	forward()
+    frontLeft.setSpeed(speed)
+    backLeft.setSpeed(speed)
+    forward()
 
 def turnLeft():
     frontRight.setSpeed(speed)
     backRight.setSpeed(speed)
-   	forward()
+    forward()
 
 def initialize():
-   	frontRight.setSpeed(100)
-   	frontLeft.setSpeed(100)
-   	backLeft.setSpeed(100)
-   	backRight.setSpeed(100)
-   	frontRight.run(Raspi_MotorHAT.FORWARD)
-   	frontLeft.run(Raspi_MotorHAT.FORWARD)
-   	backLeft.run(Raspi_MotorHAT.BACKWARD)
-   	backRight.run(Raspi_MotorHAT.BACKWARD)
-   	time.sleep(1)
+    frontRight.setSpeed(100)
+    frontLeft.setSpeed(100)
+    backLeft.setSpeed(100)
+    backRight.setSpeed(100)
+    frontRight.run(Raspi_MotorHAT.FORWARD)
+    frontLeft.run(Raspi_MotorHAT.FORWARD)
+    backLeft.run(Raspi_MotorHAT.BACKWARD)
+    backRight.run(Raspi_MotorHAT.BACKWARD)
+    time.sleep(1)
 
-initialize()
 turnLeft()
 time.sleep(duration)
 stop()
