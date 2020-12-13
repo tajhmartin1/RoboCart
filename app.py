29018800b93c9d9fdd7dabfed55a06a3f5b0c41e
Tajh Martin <tajhmartin87@gmail.com>
2020-12-12 20:16:20 -0600

    changes

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello():
-	return "it worked"
-if __name__ =="__main__":
+def showCOntroller():
+	return render_template('controller.html')
+if __name__ == "__main__":
 	app.run()
-
