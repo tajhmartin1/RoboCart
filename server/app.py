a775e0aa8fe5bfc2bd7880a5a29cc30d322091dd
Tajh Martin <tajhmartin87@gmail.com>
2021-02-18 13:50:01 -0600

    idk

--- server/app.py
+++ server/app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+# import time
+# import os
+from Raspi_MotorHAT import motorTest
 
-app = Flask(__name__)
-socketio = SocketIO(app)
-
-num=5
-@app.route('/test', methods =['POST'])
-def update():
-    num = 2
 @app.route("/")
-def showController():
-	return render_template('controller.html', x=num)
-if __name__ =="__main__":
-	socketio.run(app)
+def showCOntroller():
+	return "it worked"
+	# return render_template('controller.html')
 
-@socketio.on('connect')
-def connect():
-    print("client has connected")
-    # power up the robot
+@app.route("/right")
+def turnRight():
+    motorTest.test()
+	# frontRight.setSpeed(50)
+	# frontRight.run(Raspi_MotorHAT.FORWARD)
+	# time.sleep(1)
+	# frontRight.run(Raspi_MotorHAT.RELEASE)
+
+if __name__ == "__main__":
+	app.run()
+
